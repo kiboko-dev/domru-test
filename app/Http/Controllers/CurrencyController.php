9a10c4b8bab8f29e8bdc5d5bbe68e924e283ff8e
Kiboko <spam@runum.ru>
2020-11-19 10:01:00 +0300

    Закоментированый код

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -9,40 +9,11 @@ use Illuminate\Support\Facades\Http;
 
 class CurrencyController extends Controller
 {
-    public static function getRates(string $code)
-    {
-        $url = 'https://www.cbr-xml-daily.ru/daily_json.js';
-        $data = Http::get($url);
-        $rates = json_decode($data->body())->Valute;
-
-        return round($rates->{$code}->Value, 2);
-
-//        return [
-//            'today' =>  round($rates->{$code}->Value,2),
-//            'yesterday' =>  round($rates->{$code}->Previous,2),
-//            'ratio' =>  $rates->{$code}->Nominal
-//        ];
-    }
-
-    public function updateRates()
-    {
-        $currencies = Currency::all();
-        $i = 0;
-        foreach ($currencies as $currency) {
-            $currency->update(['amount' => self::getRates($currency->code)]);
-            $i++;
-        }
-        return true;
-    }
-
-    public function calc(string $fromCode, string $toCode, float $amount): float
-    {
-        $from = Currency::where('code', $fromCode)->first();
-        $to = Currency::where('code', $toCode)->first();
-
-        return round($amount * ($from->amount / $to->amount), 2);
-    }
-
+    /**
+     * Экшен
+     * @param ConvertRequest $request
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
     public function convert(ConvertRequest $request)
     {
 //        dd($request->all());
@@ -57,6 +28,65 @@ class CurrencyController extends Controller
         ]);
     }
 
+    /**
+     * Отображение путой формы
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function welcome()
+    {
+        return view('welcome')->with([
+            'resultAmount' => null,
+            'curList' => self::getCurList()
+        ]);
+    }
+
+    /**
+     * Получение актуального курса по ISO коду валюты
+     * @param string $code
+     * @return float
+     */
+    public static function getRates(string $code)
+    {
+        $url = 'https://www.cbr-xml-daily.ru/daily_json.js';
+        $data = Http::get($url);
+        $rates = json_decode($data->body())->Valute;
+
+        return round($rates->{$code}->Value, 2);
+    }
+
+    /**
+     * Обновление курсов валют (задел на будущее)
+     * @return bool
+     */
+    public function updateRates()
+    {
+        $currencies = Currency::all();
+        $i = 0;
+        foreach ($currencies as $currency) {
+            $currency->update(['amount' => self::getRates($currency->code)]);
+            $i++;
+        }
+        return true;
+    }
+
+    /**
+     * Метод конвертации
+     * @param string $fromCode
+     * @param string $toCode
+     * @param float $amount
+     * @return float
+     */
+    public function calc(string $fromCode, string $toCode, float $amount): float
+    {
+        $data = self::getCurrenciesData($fromCode, $toCode);
+        return round($amount * ($data['from']['amount'] / $data['to']['amount']), 2);
+    }
+
+    /**
+     * Символы валюты по коду
+     * @param string $code
+     * @return string
+     */
     public static function getSymbol(string $code): string
     {
         $symbol = [
@@ -67,20 +97,21 @@ class CurrencyController extends Controller
         return $symbol[$code];
     }
 
-
-    public function welcome()
-    {
-        return view('welcome')->with([
-            'resultAmount' => null,
-            'curList' => self::getCurList()
-        ]);
-    }
-
+    /**
+     * Список кодов валют в базе
+     * @return array
+     */
     public static function getCurList(): array
     {
         return Currency::all()->pluck('code', 'code')->toArray();
     }
 
+    /**
+     * Получение курса валюты конвертации
+     * @param string $fromCode
+     * @param string $toCode
+     * @return float
+     */
     public static function getRate(string $fromCode, string $toCode): float
     {
         $data = self::getCurrenciesData($fromCode, $toCode);
@@ -88,7 +119,7 @@ class CurrencyController extends Controller
     }
 
     /**
-     * Метод
+     * Метод получение данных валют из базы
      * @param string $fromCode
      * @param string $toCode
      * @return array
